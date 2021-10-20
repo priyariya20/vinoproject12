50d1581383d9e788ef1a45cb755e799daf9433f4
priyariya20 <priyariya.vlr97@gmail.com>
2021-10-20 13:08:27 +0530

    rithika's update code

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -2,6 +2,9 @@ package org.sample;
 
 public class Sample {
 	public static void main(String[] args) {
+		System.out.println("Welcome");
+		System.out.println("you");
+		System.out.println("back");
 		
 	}
 
